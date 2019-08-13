802fb102dd3225ce3b2486209d3010ed45e40f1b
tandaly <tandaly@qq.com>
2019-08-13 12:13:44 +0800

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,4 @@
-docker --restart=always --privileged=true --name tomcat -d -p 10080:8080 \
+docker run --restart=always --privileged=true --name tomcat -d -p 10080:8080 \
 -v /usr/local/tomcat/logs \
 --label aliyun.logs.catalina=stdout \
 --label aliyun.logs.access=/usr/local/tomcat/logs/localhost_access_log.*.txt \
